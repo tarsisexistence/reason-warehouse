e3192ee6b7471f84de05c3aa136d01846685661a
maktarsis <tarsis.maksym@gmail.com>
2020-05-01 13:13:54 +0300

    apply project preparation

--- UNUSED_webpack.config.js
+++ UNUSED_webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: './lib/js/src/Index.bs.js',
   // If you ever want to use webpack during development, change 'production'
   // to 'development' as per webpack documentation. Again, you don't have to
   // use webpack or any other bundler during development! Recheck README if
