96c975bcef2b493e1118286b0089b737a7ef302d
maktarsis <tarsis.maksym@gmail.com>
2020-05-17 10:09:20 +0300

    chore: remove analyzer, small config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -20,8 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
-    }),
-    new BundleAnalyzerPlugin()
+    })
   ],
   module: {
     rules: [
@@ -36,6 +34,7 @@ module.exports = {
     publicPath: '/',
     contentBase: '/',
     compress: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    open: true
   }
 };
